ba3e4b0e8dd6839b736a746c34b2a16807a5dff4
yeon-jae <lisanamyj99@gmail.com>
2025-03-21 16:39:25 +0900

    style: second section style

--- dfluid-website/src/components/FullBleedSection.jsx
+++ dfluid-website/src/components/FullBleedSection.jsx
@@ -42,7 +42,12 @@ function FullBleedSection() {
       <div className="bleed-Text">
         <h4>Sed ut perspiciatis unde omnis</h4>
         <p className="bleed-content">
-          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+        There are many variations of passages of Lorem Ipsum available, 
+        but the majority have suffered alteration in some form, by injected humour, 
+        or randomised words which don't look even slightly believable. 
+        If you are going to use a passage of Lorem Ipsum, 
+        you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+        All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary.
         </p>
         <div className="bleed-line"></div>
         <p className="bleed-smallText">
